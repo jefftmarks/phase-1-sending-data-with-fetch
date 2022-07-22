e4da0b0bd42a0976981b9bc953b3cd76b620e437
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-21 20:52:38 -0400

    commit tree 198c362bb07f73b99ba1aaef6e293b7ae161f391

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(res => res.json())
+    .then(object => {
+        let h1 = document.createElement('h1');
+        h1.textContent = object.id;
+        document.body.appendChild(h1);
+    })
+    .catch(error => {
+        let h1 =document.createElement('h1');
+        h1.textContent = error.message;
+        document.body.appendChild(h1);
+    })
+}
